d52533f5a9772aef8bd09e05df552282c0206c90
maguilarrdeusto <martin.aguilar@opendeusto.es>
2025-02-13 12:32:07 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class Settings(BaseSettings):
     SECRET_KEY: str = os.getenv("SECRET_KEY", "clave_por_defecto")
-    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./test.db")
+    DATABASE_URL: str = os.getenv("DATABASE_URL").replace("postgres://", "postgresql+asyncpg://", 1)
     OPTIMIZATION_SERVICE_URL: str = os.getenv("OPTIMIZATION_SERVICE_URL", "http://127.0.0.1:5000/optimize")
     OPTIMIZATION_MODE: str = os.getenv("OPTIMIZATION_MODE", "test")
 
